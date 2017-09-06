796f0c945d346da4444923e542c8ad5e36a4c8ee
jaimemor <31485731+jaimemor@users.noreply.github.com>
2017-09-06 15:58:40 -0400

    ultimo avance

--- 2 clase/procesar.php
+++ 2 clase/procesar.php
@@ -6,11 +6,15 @@ $nombre=$_REQUEST['nombre'];
 $apellido=$_REQUEST['apellido'];
 $rut=$_REQUEST['rut'];
 
+}else{
+	echo "gg";
 }
 
 if(isset($_REQUEST['ramo_form'])){
 $ramo=$_REQUEST['ramo'];
 $nota=$_REQUEST['nota'];
 
+}else{
+	echo "gg2";
 }
 ?>
